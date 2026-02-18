ac2bbc392b444cd854274a709dd7d3190522fa15
AmirHesam Ghahari <ahesam.ghahari@gmail.com>
2026-02-18 14:13:43 -0500

    Add built site from Plasmic

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  // Enable static export (for S3 hosting)
+  output: "export",
+
+  // Required for static hosting (no Next.js image server)
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
